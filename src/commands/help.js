603450bdd93e67e8c2a60022596c03efefc8ab1a
adsk <kirillbaluca@gmail.com>
2025-04-08 02:59:06 +0300

    добавлена команда "now" в пункте помощи

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,7 +1,7 @@
 terminal.addCommand("help", function() {
     terminal.printLine("Добро пожаловать в раздел помощи!", Color.COLOR_1)
     terminal.printLine("Доступные команды:\n")
-    let helpCommands = ["helloworld", "clear"]
+    let helpCommands = ["helloworld", "now", "clear"]
     let longestCommandLength = helpCommands.reduce((p, c) => Math.max(p, c.length), 0)
     for (let command of helpCommands.sort((a, b) => a.localeCompare(b))) {
         let spaces = strRepeat(" ", longestCommandLength - command.length + 2)
